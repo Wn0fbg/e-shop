11e3fb05ed7c162748a805d5c35cd62f5f645cda
Wn0fbg <temakozlovofficiall@gmail.com>
2025-12-31 21:11:05 +0300

    finished page shop

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -6,6 +6,7 @@ import Shop from "./pages/Shop";
 import ShopCategory from "./pages/ShopCategory";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignUp />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
