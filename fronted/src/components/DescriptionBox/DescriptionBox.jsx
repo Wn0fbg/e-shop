5e99eb4b947309db4dcc8984f3b23a5f7c35e0e7
Wn0fbg <temakozlovofficiall@gmail.com>
2026-01-02 20:27:27 +0300

    section DescriptionBox for page Product

---
+++ fronted/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="description_box">
+      <div className="description_box-navigator">
+        <div className="description-nav-box">Description</div>
+        <div className="description-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="description_box-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+          mollitia. Nemo nobis iste laudantium unde veritatis quasi quas, illum
+          rerum? Fugiat tempore, veniam obcaecati autem modi quaerat accusamus,
+          inventore ipsum, saepe amet ex. Qui enim quam voluptates nam quos
+          officiis aliquam architecto distinctio at, facere minus! Placeat quod
+          numquam esse laboriosam recusandae tempore sunt mollitia quaerat
+          sapiente dolore. Eius, nihil.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          voluptatum totam quae quisquam explicabo? Tenetur porro dignissimos
+          obcaecati sit neque?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
