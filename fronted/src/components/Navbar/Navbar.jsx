fddcbc888e2dc3a08212133a3457707ff27a1e42
Wn0fbg <temakozlovofficiall@gmail.com>
2026-01-03 19:43:31 +0300

    adaptive for navbar

--- fronted/src/components/Navbar/Navbar.jsx
+++ fronted/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import cart_icon from "../assets/cart_icon.png";
